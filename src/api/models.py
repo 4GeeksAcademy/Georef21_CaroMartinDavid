6658f973a81c009788535b8fefba42dcc59fa0a9
Martin Gallo <136949107+MRTN74@users.noreply.github.com>
2023-10-28 03:25:15 +0000

    Changes

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,23 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Specialist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    profesion = db.Column(db.String(120), nullable=False)
+    area_de_desempeño = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Specialist {self.nombre} {self.apellido}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "email": self.email,
+            "profesion": self.profesion,
+            "area_de_desempeño": self.area_de_desempeño
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Specialist
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,37 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/especialista', methods=['GET'])
+def get_especialista():
+    # Supongamos que deseas obtener todos los especialistas de la base de datos
+    especialistas = Specialist.query.all()
+    
+    # Convierte los objetos Specialist en un formato serializable
+    especialistas_serializados = [especialista.serialize() for especialista in especialistas]
+    
+    return jsonify(especialistas_serializados), 200
+
+@api.route('/especialista', methods=['POST'])
+def create_especialista():
+    # Obtiene los datos del especialista desde la solicitud
+    data = request.json
+
+    if not data:
+        return jsonify({"message": "Datos no proporcionados"}), 400
+
+    # Crea un nuevo objeto Specialist
+    nuevo_especialista = Specialist(
+        nombre=data.get("nombre"),
+        # apellido=data.get("apellido"),
+        email=data.get("email"),
+        profesion=data.get("profesion"),
+        area_de_desempeño=data.get("area_de_desempeño"),
+        password=data.get("password")
+    )
+    # Agrega el especialista a la base de datos
+    db.session.add(nuevo_especialista)
+    db.session.commit()
+
+    # Puedes retornar una respuesta con el nuevo especialista creado
+    return jsonify({"message": "Especialista creado con éxito", "id": nuevo_especialista.id}), 201
