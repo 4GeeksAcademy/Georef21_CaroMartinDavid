3858447841e7a2908a2615cbc453094706b440de
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-11-01 23:57:07 +0000

    react modulo admon v3

--- src/front/js/pages/datosadmons.js
+++ src/front/js/pages/datosadmons.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import {NavbarPerfilAdmon} from "../component/navbarperfiladmon"
+import "../../styles/admon.css";
 
 import { Context } from "../store/appContext";
 import { FaLocationDot, FaPhoneFlip, FaEnvelope, FaTrashCan,FaPencil } from "react-icons/fa6";
@@ -17,7 +18,7 @@ export const DatosAdmon = () => {
 				{store.administrators.map((item, index)=>{
 					return(
 						<div key={index}
-							className="col-md-12 mx-auto">
+							className="col-md-12 mx-auto admon">
 							<h1>{item.name} {item.lastname}</h1>
 							<button className="editor p-2 mx-3"><FaPencil/></button>
 							<button className="delete p-2 mx-3"><FaTrashCan/></button>
