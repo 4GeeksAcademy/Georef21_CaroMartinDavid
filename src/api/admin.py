a705d7173ac9779c198c708ce614811103d11a0e
Dher82 <126974687+Dher82@users.noreply.github.com>
2023-11-07 01:40:09 +0000

    -rama merge Módulo proyecto y Módulo Administrador

--- src/api/admin.py
+++ src/api/admin.py
@@ -12,7 +12,8 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Project, db.session))    admin.add_view(ModelView(Administrator, db.session))
+    admin.add_view(ModelView(Project, db.session))
+    admin.add_view(ModelView(Administrator, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
