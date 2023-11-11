afc7a6cc7717d08e80015327754d38bcdefd5cb1
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-11-11 14:00:58 +0000

    perfil admon

--- src/front/js/component/navbarperfiladmon.js
+++ src/front/js/component/navbarperfiladmon.js
@@ -1,19 +1,32 @@
-
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { BsPersonCircle } from "react-icons/bs";
 
 export const NavbarPerfilAdmon = () => {
 	const { store, actions } = useContext(Context);
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container d-flex justify-content-end">
+		<div className="d-flex justify-content-center">
+		<nav className="navbar navbar-light bg-light col-md-10 d-flex justify-content-end">
+			<div >
 				
-				<div className="buttonlogOut ">
+				<div className="d-flex justify-content-center ">
 					{store.session === true ?
 					<>
+						
+						<div className="dropdown">
+							<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							<BsPersonCircle/> {store.administrator.name} {store.administrator.lastname}
+							</a>
+
+							<ul className="dropdown-menu">
+								<li><a className="dropdown-item" href="#">Mi Perfil</a></li>
+								<li><a className="dropdown-item" href="#">Another action</a></li>
+								<li><a className="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+						</div>
 						<Link to="/">
-						<button className="btn btn-danger" onClick={()=>{actions.logout()}}>Log Out </button>
+						<button className="btn btn-danger mx-3 px-1" onClick={()=>{actions.logout()}}>Log Out </button>
 						</Link>
 					</>
 					:<span></span>
@@ -21,5 +34,6 @@ export const NavbarPerfilAdmon = () => {
 				</div>
 			</div>
 		</nav>
+		</div>
 	);
 };
