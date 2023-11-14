bbc9735e3416ec7a4ddd6cdbb61f4c8b391ec336
Martin Gallo <136949107+MRTN74@users.noreply.github.com>
2023-11-14 01:34:42 +0000

    finalizado

--- src/front/js/pages/tomaDeDatos.js
+++ src/front/js/pages/tomaDeDatos.js
@@ -28,7 +28,7 @@ export const DataCaptureRegister = () => {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                },
+                },                                      
                 body: JSON.stringify(dataCaptureData),
             });
 
