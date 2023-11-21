165797ffe8d96ec419f5f369d4e4cd1ff45c8294
Martin Gallo <136949107+MRTN74@users.noreply.github.com>
2023-11-21 01:42:52 +0000

    112023

--- src/front/js/component/navbarperfiladmon.js
+++ src/front/js/component/navbarperfiladmon.js
@@ -67,52 +67,52 @@ export const NavbarPerfilAdmon = () => {
 							<a className="nav-link" href="logSpecialist">Especialista</a>
 						</li>
 						{store.session === true ?
-						<>
-						<div className="dropdown">
-							<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-								<BsPersonCircle /> {store.administrator.name} {store.administrator.lastname}
-							</a>
+							<>
+								<div className="dropdown">
+									<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										<BsPersonCircle /> {store.administrator.name} {store.administrator.lastname}
+									</a>
 
-							<ul className="dropdown-menu">
-								<Link to="/admons" style={{ textDecoration: 'none', color: 'black' }}>
-									<span>Mi Perfil</span>
+									<ul className="dropdown-menu">
+										<Link to="/admons" style={{ textDecoration: 'none', color: 'black' }}>
+											<span>Mi Perfil</span>
+										</Link>
+										<li><a className="dropdown-item p-0" href="#" onClick={() => { actions.openModaldelete() }}>Eliminar Cuenta</a></li>
+										<Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
+											<span onClick={() => { actions.logout() }}> Cerrar sesión</span>
+										</Link>
+									</ul>
+								</div>
+								<Link to="/">
+									<button className="btn btn-danger mx-3 px-1" onClick={() => { actions.logout() }}>Log Out </button>
 								</Link>
-								<li><a className="dropdown-item p-0" href="#" onClick={() => { actions.openModaldelete() }}>Eliminar Cuenta</a></li>
-								<Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
-									<span onClick={() => { actions.logout() }}> Cerrar sesión</span>
-								</Link>
-							</ul>
-						</div>
-						<Link to="/">
-							<button className="btn btn-danger mx-3 px-1" onClick={() => { actions.logout() }}>Log Out </button>
-						</Link>
-						<ModalEliminarAdmon id={store.administrator.id} />
-						</>
-						: <span></span>}
+								<ModalEliminarAdmon id={store.administrator.id} />
+							</>
+							: <span></span>}
 						{store.sessionSpecialist === true ?
-                             <>
+							<>
 
-                                 <div className="dropdown">
-                                     <a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                         <BsPersonCircle /> {store.specialist.nombre} {store.specialist.apellido}
-                                     </a>
+								<div className="dropdown">
+									<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										<BsPersonCircle /> {store.specialist.nombre} {store.specialist.apellido}
+									</a>
 
-                                     <ul className="dropdown-menu">
-                                         <li><Link to="/datosEspecialista">
-                                             <span>Mi Perfil</span>
-                                         </Link></li>
-                                         <li><Link to="/">
-                                             <span onClick={() => { actions.logoutSpecialist() }}> Cerrar sesión</span>
-                                         </Link></li>
-                                     </ul>
-                                 </div>
-                                 <Link to="/">
-                                     <button className="btn btn-danger mx-3 px-1" onClick={() => { actions.logoutSpecialist() }}>Log Out </button>
-                                 </Link>
+									<ul className="dropdown-menu">
+										<li><Link to="/datosEspecialista">
+											<span>Mi Perfil</span>
+										</Link></li>
+										<li><Link to="/">
+											<span onClick={() => { actions.logoutSpecialist() }}> Cerrar sesión</span>
+										</Link></li>
+									</ul>
+								</div>
+								<Link to="/">
+									<button className="btn btn-danger mx-3 px-1" onClick={() => { actions.logoutSpecialist() }}>Log Out </button>
+								</Link>
 
-                             </>
-                             : <span></span>
-                         }
+							</>
+							: <span></span>
+						}
 					</ul>
 				</div>
 			</div>
