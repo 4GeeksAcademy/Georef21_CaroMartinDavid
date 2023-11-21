797fe53a5ef209ebf719ef2f38b0b00fdbaf1346
Martin Gallo <136949107+MRTN74@users.noreply.github.com>
2023-11-21 03:14:09 +0000

    Navbar final y card de especialista

--- src/front/js/pages/perfilEspecialista.js
+++ src/front/js/pages/perfilEspecialista.js
@@ -5,40 +5,65 @@ import { Context } from "../store/appContext";
 export const PerfilEspecialista = () => {
     const { store, actions } = useContext(Context);
     const navigate = useNavigate();
-    
-    function eliminaresp(id){
+
+    function eliminaresp(id) {
         actions.eliminarEspecialista(id);
         navigate("/profileadmon")
     }
-  
+
     return (
         <div className="container">
-            {store.allspecialist.length==0 ? <h1>No tienes especialistas</h1>:
-            <>
-                {store.allspecialist.map((item, index) => {
-                    return (
-                        <div className="card" key={index}>
-                            <img src="https://static.vecteezy.com/system/resources/thumbnails/007/319/933/small/black-avatar-person-icons-user-profile-icon-vector.jpg" className="img-fluid w-25" alt="..." />
-                            <div className="card-body">
-                                <h3>nombre: {item.nombre}</h3>
-                                <h3>apellido: {item.apellido}</h3>
-                                <h3>email: {item.email}</h3>
-                                <h3>area_de_desempeno: {item.area_de_desempeno}</h3>
-                                <h3>profesion: {item.profesion}</h3>
-                                <button onClick={() => eliminaresp(item.id)}>Eliminar</button>
-                                <Link to={`/registerespicialist/${item.id}`}>
-                                    <button >Editar</button>
-                                </Link>
+            {store.allspecialist.length == 0 ? <h1>No tienes especialistas</h1> :
+                <>
+                    {store.allspecialist.map((item, index) => {
+                        return (
+                            <div class="card-body">
+                                <span class="float-start m-2 me-4">
+                                    <img src="assets/images/users/avatar-2.jpg" style="height: 100px;" alt="avatar-2" class="rounded-circle img-thumbnail" />
+                                </span>
+                                <div class="">
+                                    <h4 class="mt-1 mb-1">{item.nombre}{item.apellido}</h4>
+                                    {/* <p class="font-13"> Authorised Brand Seller</p> */}
+
+                                    <ul class="mb-0 list-inline">
+                                        <li class="list-inline-item me-3">
+                                            <h5 class="mb-1">{item.email}</h5>
+                                            {/* <p class="mb-0 font-13">Total Revenue</p> */}
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <h5 class="mb-1">{item.area_de_desempeno}</h5>
+                                            <p class="mb-0 font-13">Number of Orders</p>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
-                           
-                        </div>
-                    );
-                })}
-            </>
+                        );
+                    })}
+                </>
             }
-             <Link to="/profileadmon">
+            <Link to="/profileadmon">
                 <button type="button" className="btn btn-outline-dark">Volver</button>
             </Link>
         </div>
     );
 };
+{/* <div class="card-body">
+    <span class="float-start m-2 me-4">
+        <img src="assets/images/users/avatar-2.jpg" style="height: 100px;" alt="avatar-2" class="rounded-circle img-thumbnail" />
+    </span>
+    <div class="">
+        <h4 class="mt-1 mb-1">{item.nombre}{item.apellido}</h4>
+        {/* <p class="font-13"> Authorised Brand Seller</p> */}
+
+        // <ul class="mb-0 list-inline">
+        //     <li class="list-inline-item me-3">
+        //         <h5 class="mb-1">{item.email}</h5>
+        //         {/* <p class="mb-0 font-13">Total Revenue</p> */}
+        //     </li>
+        //     <li class="list-inline-item">
+        //         <h5 class="mb-1">{item.area_de_desempeno}</h5>
+        //         <p class="mb-0 font-13">Number of Orders</p>
+        //     </li> */}
+        {/* </ul>
+    </div>
+</div> */}
