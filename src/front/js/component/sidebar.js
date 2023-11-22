46a4ae11ecfba6c173c2c650508b2ae87592001e
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-11-22 03:13:56 +0000

    cambios especialista

--- src/front/js/component/sidebar.js
+++ src/front/js/component/sidebar.js
@@ -143,20 +143,20 @@ export const Sidebar = () => {
             <div>
                 <div className="d-flex align-items-center pb-1 mb-1 link-dark text-decoration-none border-bottom">
                     <a href="#!" className="text-decoration-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-globe-americas" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#727cf5" className="bi bi-globe-americas" viewBox="0 0 16 16">
                             <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z" />
                         </svg>
-                    </a> <span className="fs-5 fw-semibold">Bienvenido</span>
+                    </a> <span className="fs-3 fw-semibold px-2">Bienvenido</span>
                 </div>
-                <ul className="list-unstyled ps-0">
+                <ul className="list-unstyled py-2">
                     <li className="mb-1">
-                        <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+                        <button className="btn btn-toggle align-items-center rounded collapsed buttonsidebar" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
                             Proyectos
                         </button>
                         <div className="collapse" id="home-collapse">
                             <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                                 <li>
-                                        <button type="button" className="btn btn-primary m-3" onClick={()=>projecasig()}>
+                                        <button type="button" className="btn btn-outline-secondary m-3 buttonsidebar1" onClick={()=>projecasig()}>
                                            Proyectos Asignados
                                         </button>
                             
@@ -166,13 +166,13 @@ export const Sidebar = () => {
                     </li>
 
                     <li className="mb-1">
-                        <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+                        <button className="btn btn-toggle align-items-center rounded collapsed buttonsidebar" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
                             Visitas
                         </button>
                         <div className="collapse" id="home-collapse">
                             <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                                 <li className="mb-1">
-                                    <button type="button" className="btn btn-primary m-3" onClick={()=>visitasig()}>
+                                    <button type="button" className="btn btn-outline-secondary m-3 buttonsidebar1" onClick={()=>visitasig()}>
                                         Visitas Asignadas
                                     </button>
                                 </li>
@@ -180,13 +180,13 @@ export const Sidebar = () => {
                         </div>
                     </li>
                     <li className="mb-1">
-                        <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+                        <button className="btn btn-toggle align-items-center rounded collapsed buttonsidebar" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
                             Datos Capturados 
                         </button>
                         <div className="collapse" id="home-collapse">
                             <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                                 <li className="mb-1">
-                                    <button type="button" className="btn btn-primary m-3" onClick={()=>datacapt()}>
+                                    <button type="button" className="btn btn-outline-secondary m-3 buttonsidebar1" onClick={()=>datacapt()}>
                                         Datos capturados
                                     </button>
                                 </li>
---
+++ src/front/js/pages/mapadmon.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useContext, useRef } from "react"; 
+import { Context } from "../store/appContext";
+import { GoogleMap, Marker, useLoadScript  } from "@react-google-maps/api";
+import { useMemo } from "react";
+import "../../styles/map.css";
+
+export const MapAdmon = () => {
+  const { store, actions } = useContext(Context);
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.API_KEY
+  });
+  const center = useMemo(() => (store.location), [store.location]);
+  const [map, setMap] = useState(null);
+  const [markerPosition, setMarkerPosition] = useState(store.location);
+  
+
+  useEffect(() => {
+    setMarkerPosition(store.location);
+  }, [store.location]);
+
+  useEffect(() => {
+    actions.ajustLocation(markerPosition);
+  }, [markerPosition]);
+
+  const onLoad = mapInstance => {
+    setMap(mapInstance);
+  };
+
+  const onMarkerDragEnd = event => {
+    
+    setMarkerPosition(prevPosition => ({
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+    }));
+    console.log("markerposition", markerPosition);
+    onCoordinateChange(markerPosition);
+    
+  };
+
+  const onMapClick = event => {
+    
+    setMarkerPosition(prevPosition => ({
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+    }));
+    console.log("markerposition", markerPosition);
+    onCoordinateChange(markerPosition);
+   
+  };
+
+ 
+  return (
+    
+      <div className="App">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          onLoad={onLoad}
+          center={center}
+          zoom={10}
+          onClick={onMapClick}
+          >
+          <Marker position={markerPosition} icon={"https://maps.google.com/mapfiles/ms/icons/green-dot.png"} draggable={true}
+            onDragEnd={onMarkerDragEnd} />
+
+        
+        </GoogleMap>
+      )}
+    </div>
+   
+  );
+};
