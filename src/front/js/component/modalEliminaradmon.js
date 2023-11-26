5b280eff3c1063cb38d601ab8c8e5ac582565ad9
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-11-26 02:31:46 +0000

    ajustes modal

--- src/front/js/component/modalEliminaradmon.js
+++ src/front/js/component/modalEliminaradmon.js
@@ -9,6 +9,7 @@ export const ModalEliminarAdmon = (prop) => {
 
 function deleteadmin(id){
     actions.deleteadmon(id);
+    actions.getadmins();
     actions.closeModaldelete();
     actions.logout();
     navigate("/");
@@ -19,14 +20,14 @@ return (
         <div className="modal-dialog" role="document">
             <div className="modal-content">
             <div className="modal-header">
-                <h5 className="modal-title">Eliminar mi cuenta</h5>
+                <h5 className="modal-title" style={{fontSize:"15px", color:"black"}}>Eliminar cuenta</h5>
                     <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={()=>actions.closeModaldelete()} >
                     <span aria-hidden="true">&times;</span>
                     </button>
                 
             </div>
             <div className="modal-body">
-                <p>Si quieres eliminar tu cuenta podemos ayudarte a hacerlo. Ten en cuenta que no podras recuperar ningun contenido que hayas subido a tu cuenta</p>
+                <p style={{fontSize:"13px", color:"black"}}>Si quieres eliminar tu cuenta podemos ayudarte a hacerlo. Ten en cuenta que no podras recuperar ningun contenido que hayas subido a tu cuenta</p>
             </div>
             <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-dismiss="modal"  onClick={()=>deleteadmin(prop.id)}>Aceptar</button>
