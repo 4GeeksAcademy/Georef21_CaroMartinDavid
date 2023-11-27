6650d33f47642f0df4ad59dc3ecd630c11c995f8
Martin Gallo <136949107+MRTN74@users.noreply.github.com>
2023-11-27 20:41:47 +0000

    changes

--- src/front/js/component/navbarperfiladmon.js
+++ src/front/js/component/navbarperfiladmon.js
@@ -4,6 +4,8 @@ import { Context } from "../store/appContext";
 import { BsPersonCircle } from "react-icons/bs";
 import { ModalEliminarAdmon } from "./modalEliminaradmon";
 import LogoGeoref21 from '../../img/LogoGeoref21.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 export const NavbarPerfilAdmon = () => {
 	const { store, actions } = useContext(Context);
@@ -13,26 +15,26 @@ export const NavbarPerfilAdmon = () => {
 		<nav className="navbar navbar-expand-lg Navbarhome" >
 			<div className="container-fluid"  >
 				<a className="navbar-brand" href="/">
-					<img src={LogoGeoref21} width="135" height="40" alt="Logo" />
+					<img src={LogoGeoref21} width="135" height="36" alt="Logo" />
 				</a>
-				<div className="collapse navbar-collapse d-flex" id="navbarNav" style={{marginLeft:"20px"}} >
+				<div className="collapse navbar-collapse d-flex" id="navbarNav" style={{ marginLeft: "20px" }} >
 					<ul className="navbar-nav" style={{ marginRight: "20px" }}>
 						{store.session === true || store.sessionSpecialist === true ? <span></span> :
-						<>
-												<li className="nav-item">
-							<a className="nav-link active navInicio" aria-current="page" href="/">Inicio</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="admonLog">Administrador</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="logSpecialist">Especialista</a>
-						</li>
-						</>}
+							<>
+								<li className="nav-item">
+									<a className="nav-link active navInicio" aria-current="page" href="/">Inicio</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="admonLog">Administrador</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="logSpecialist">Especialista</a>
+								</li>
+							</>}
 						{store.session === true ?
 							<>
-								<div className="dropdown" style={{marginLeft:"1400px"}}>
-									<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								<div className="dropdown" style={{ marginLeft: "1400px" }}>
+									<a className="btn buttonHome dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 										<BsPersonCircle /> {store.administrator.name} {store.administrator.lastname}
 									</a>
 
@@ -47,7 +49,10 @@ export const NavbarPerfilAdmon = () => {
 									</ul>
 								</div>
 								<Link to="/">
-									<button className="btn btn-danger mx-3 px-1" onClick={() => { actions.logout() }}>Log Out </button>
+									<button className="btn btn-danger mx-3 px-1" style={{borderRadius:"10px"}} onClick={() => { actions.logoutSpecialist() }}>
+										<FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+										Salir
+									</button>
 								</Link>
 								<ModalEliminarAdmon id={store.administrator.id} />
 							</>
@@ -55,8 +60,8 @@ export const NavbarPerfilAdmon = () => {
 						{store.sessionSpecialist === true ?
 							<>
 
-								<div className="dropdown" style={{marginLeft:"1370px"}}>
-									<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								<div className="dropdown" style={{ marginLeft: "1370px" }}>
+									<a className="btn buttonHome dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 										<BsPersonCircle /> {store.specialist.nombre} {store.specialist.apellido}
 									</a>
 
@@ -70,7 +75,10 @@ export const NavbarPerfilAdmon = () => {
 									</ul>
 								</div>
 								<Link to="/">
-									<button className="btn btn-danger mx-3 px-1" onClick={() => { actions.logoutSpecialist() }}>Log Out </button>
+									<button className="btn btn-danger mx-3 px-1" style={{borderRadius:"10px"}} onClick={() => { actions.logoutSpecialist() }}>
+										<FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+										Salir
+									</button>
 								</Link>
 
 							</>
